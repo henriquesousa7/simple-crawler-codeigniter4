68bf9a4848e518c6da73d04598bb47f6be9fa3f5
henriquesousa7 <henrique.devsousa@gmail.com>
2020-09-29 13:29:56 -0300

    added search function

--- app/Views/mainViews/header.php
+++ app/Views/mainViews/header.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <title>Crawler Alura Cursos</title>
     <style>
-        .centered {
-            margin: 0 auto !important;
-            float: none !important;
+        #well {
+            width: 50%;
+            margin: 15px;
+            transform: translate(50%,50%);
         }
     </style>
 </head>
--- app/Views/mainViews/view.php
+++ app/Views/mainViews/view.php
@@ -2,14 +2,48 @@
     <h2>Cursos PHP - Alura</h2>
     <a href="/cursos" class="btn btn-primary">Retornar para pagina inicial</a>
     <a href="/cursos/exportCSV" class="btn btn-primary">Exportar para arquivo CSV</a>
+
+    <div id="well">
+      	<input id="search" type="text" class="form-control" placeholder="Search">
+    </div>
+
     <table class="table">
-        <tr>
-            <th>Nome</th>
-        </tr>
-        <?php foreach ($cursos as $curso):?>
+        <thead>
             <tr>
-                <td><?php echo $curso['nome'] ?></td>
+                <th>Nome</th>
             </tr>
-        <?php endforeach; ?>
+        </thead>
+        <tbody>
+            <?php foreach ($cursos as $curso):?>
+                <tr>
+                    <td><?php echo $curso['nome'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
     </table>
+
+    <script>
+
+        function search_table(value){
+            $('tbody tr').each(function(){
+                var found = 'false';
+                $(this).each(function(){
+                    if($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0){
+                        found = 'true';                    
+                    }
+                })
+                if(found == 'true'){
+                    $(this).show()
+                }else{
+                    $(this).hide()
+                }
+            })
+        }
+
+        $('#search').keyup(function(){
+            search_table($(this).val());
+            console.log($(this).val())
+        })
+
+    </script>
 </div>
